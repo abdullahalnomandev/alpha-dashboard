c79b87f014ca19493a2f5a7bc573114b061b6be5
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2026-04-02 16:48:41 +0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-
-  return {
-    plugins: [
-      react({
-        babel: {
-          plugins: [["babel-plugin-react-compiler"]],
-        },
-      }),
-    ],
-    server: {
-      host: true
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+})
